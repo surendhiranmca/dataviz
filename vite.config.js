9f197d48ef13dc4ea0faf3d895e596bc769cca05
Surendhiran A <surendhitan@gmail.com>
2026-07-31 11:00:23 +0530

    Configure port binding 0.0.0.0 for Render

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    host: '0.0.0.0',
+    port: process.env.PORT ? parseInt(process.env.PORT) : 10000,
+    allowedHosts: true,
+  },
+  server: {
+    host: '0.0.0.0',
+    port: 5173,
+  }
 })
